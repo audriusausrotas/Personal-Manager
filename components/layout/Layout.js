72e67ebebccc090ebc75ca3f6224fca13ed1f97d
sopuku <audrius.ausrotas@gmail.com>
2023-02-10 13:56:13 +0200

    minor styling added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,8 +1,16 @@
 import Nav from "./Nav";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Image
+        className="background"
+        alt="beautiful abstract background"
+        src="/bg.jpg"
+        width={2048}
+        height={1152}
+      />
       <Nav />
       <main>{children}</main>
     </>
